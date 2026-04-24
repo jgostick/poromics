4d6449cfc725deb89e8cc305d9c3a65da73c8841
Jeff Gostick <jgostick@gmail.com>
2026-04-24 00:51:15 -0400

    changing s3 policy

--- apps/web/storage_backends.py
+++ apps/web/storage_backends.py
@@ -5,13 +5,13 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class PublicMediaStorage(S3Boto3Storage):
     location = "media"
-    default_acl = "public-read"
+    default_acl = None
     file_overwrite = False
 
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = "private"
-    default_acl = "private"
+    default_acl = None
     file_overwrite = False
     custom_domain = False
 
